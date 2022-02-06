f65b250f777ecf03e63a48af34d1681e90d58f28
Gabriel Jacintho <bieljacintho@gmail.com>
2022-02-06 14:48:30 -0300

    statistics.py

--- statistics.py
+++ statistics.py
@@ -11,7 +11,9 @@ Data: 02/02/2022
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pingouin
+import pingouin as pg
+import scipy.stats as stats
+import hypothetical as hp
 
 #%% DATA READING
 col_list = ["TP_CATEGORIA_ADMINISTRATIVA",
@@ -48,7 +50,7 @@ census_raw['REGIAO_NASCIMENTO'].replace(['PR','SC','RS'], "South", inplace = Tru
 # Defining the chi square test
 def chi_square(DataFrame, column_x, column_y, graph_title) : 
     
-    stat = pingouin.chi2_independence(df, x= column_x, y= column_y)                                         # Calculates the chi square test parameters
+    stat = pg.chi2_independence(df, x= column_x, y= column_y)                                               # Calculates the chi square test parameters
     p_value = stat[2]['pval'][0]                                                                            # Select the Pearson's P-Value
 
     # Verify if the test is significant
@@ -121,3 +123,28 @@ title= 'Students with social support by the reservation\
     of vacancies program in Brazilian Public Universities'                                                  # Set the graph title
 chi_square(df, 'IN_RESERVA_VAGAS', 'IN_APOIO_SOCIAL', title)                                                # Run the statistic test
 
+#%% STATISTICS: AGE VS RACE/COLOR
+
+df = census_raw.copy()                                                                                      # Copy Data Frame
+
+# Configuring and cleaning the Data Frame
+df.drop(df[df.TP_SITUACAO != 'Attending'].index, inplace=True)                                              # Select students that are attending only
+df.drop(df[df.TP_COR_RACA == 'No response'].index, inplace=True)                                            # Remove students that haven't answered their color/race
+df.drop(df[df.TP_COR_RACA == 'Not Declare'].index, inplace=True)                                            # Remove students that haven't declared their color/race
+
+stat = pg.normality(df, dv='NU_IDADE', group='TP_COR_RACA', method='normaltest', alpha=0.05)
+
+plt.figure(figsize =(12, 8))
+sns.set_theme(style="whitegrid")
+sns.boxplot(x= 'TP_COR_RACA', y='NU_IDADE', data=df)
+
+test = df.pivot(values='NU_IDADE', columns='TP_COR_RACA')
+# white = test['White'].dropna()
+# print(white)
+
+mood = hp.nonparametric.MedianTest(test['White'].dropna(),
+                                   test['Brown'].dropna(),
+                                   test['Black'].dropna(),
+                                   test['Indigenous'].dropna(),
+                                   test['Yellow'].dropna())
+print(mood.test_summary)
